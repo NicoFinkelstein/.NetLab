fefba503e04a0a6e4742c900ac67e1ec352c319b
Nicolas M Finkelstein <finkelsteinunl@gmail.com>
2022-06-01 13:12:01 -0300

    First commit

---
+++ ExtensionMethod/NumberModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public class NumberModel
+    {
+        public int NumberA { get; set; }
+        public int NumberB { get; set; }    
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejecutar();
+        }
+        private static void Ejecutar()
+        {
+            DivisionMachine dividir = new DivisionMachine();
+            NumberModel modelo = new NumberModel();
+            Console.WriteLine("Cargue el dividendo (numero que quiere que se divida)");
+            modelo.NumberA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cargue el divisor (numero por el cual quiere que se divida");
+            modelo.NumberB = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es : " +dividir.Dividir(modelo));
+            Console.ReadLine();
+        }
+    }
+}
