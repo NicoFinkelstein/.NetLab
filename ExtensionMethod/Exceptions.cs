04351da978968ad9868785f7825a8fa5ac25e49f
Nicolas M Finkelstein <finkelsteinunl@gmail.com>
2022-06-01 13:33:06 -0300

    Second commit

---
+++ ExtensionMethod/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public class Exceptions
+    {
+        public static void ThrowException()
+        {
+
+        }
+    }
+}
