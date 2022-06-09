fa4a7936226740d253b348d131ec4a32a28939b7
Nicolas M Finkelstein <finkelsteinunl@gmail.com>
2022-06-09 08:29:21 -0300

    New

---
+++ ExtensionMethod/DivisionMachine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public class DivisionMachine
+    {
+        public int Dividir(NumberModel modelo)
+        {
+            return modelo.NumberA / modelo.NumberB;
+        }
+    }
+
+}
---
+++ ExtensionMethod/NumberModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExtensionMethod
+{
+    public class NumberModel
+    {
+        public int NumberA { get; set; }
+        public int NumberB { get; set; }
+    }
+
+}
